5a489186fd729e018410ab9701da6af6e45fc310
Nicolas Inchausti <goldenplushd@gmail.com>
2022-11-07 12:24:30 -0300

    hasta cambiar a productos

---
+++ proyecto/proyecto/src/Components/NavBar/Data/product.js
@@ -0,0 +1,54 @@
+const products = [
+
+    {
+        id: 1,
+        title: "Producto 1",
+        description: "asdasdasd",
+        price: 200,
+        stock: 10,
+        imgurl:"https://www.pixelstalk.net/wp-content/uploads/2016/05/Coffee-Wallpapers-HD-Free-Download.jpg",
+    },
+    {
+        id: 2,
+        title: "Producto 2",
+        description: "asdasdasd",
+        price: 200,
+        stock: 10,
+        imgurl:"https://www.xtrafondos.com/wallpapers/taza-de-cafe-4307.jpg",
+    },
+    {
+        id: 3,
+        title: "Producto 3",
+        description: "asdasdasd",
+        price: 200,
+        stock: 10,
+        imgurl:"https://images3.alphacoders.com/941/94135.jpg",
+     },
+     {
+        id: 4,
+        title: "Producto 4",
+        description: "asdasdasd",
+        price: 200,
+        stock: 10,
+        imgurl:"https://images8.alphacoders.com/903/903178.jpg",
+     },
+     {
+        id: 5,
+        title: "Producto 5",
+        description: "asdasdasd",
+        price: 200,
+        stock: 10,
+        imgurl:"https://images6.alphacoders.com/869/869347.jpg",
+     },
+     {
+        id: 6,
+        title: "Producto 6",
+        description: "asdasdasd",
+        price: 200,
+        stock: 10,
+        imgurl:"https://images5.alphacoders.com/787/787947.jpg",
+     }
+
+];
+
+export default products;
--- proyecto/proyecto/src/Components/NavBar/ItemListContainer/ItemListContainer.js
+++ proyecto/proyecto/src/Components/NavBar/ItemListContainer/ItemListContainer.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import FlexWrapper from '../FlexWrapper/FlexWrapper';
 import Item from '../Item/Item';
+import products from '../Data/product';
 
 function ItemListContainer() {
   return (
     <div>
-        <Item price={200} title="Producto 1" imgurl="https://www.pixelstalk.net/wp-content/uploads/2016/05/Coffee-Wallpapers-HD-Free-Download.jpg"/>
+        <FlexWrapper>
+
+          <Item 
+          price={200} 
+          title="Producto 1"
+          imgurl="https://www.pixelstalk.net/wp-content/uploads/2016/05/Coffee-Wallpapers-HD-Free-Download.jpg"/>
+
+          <Item 
+          price={350} 
+          title="Producto 2" 
+          imgurl="https://www.xtrafondos.com/wallpapers/taza-de-cafe-4307.jpg"/>
+
+          <Item 
+          price={950} 
+          title="Producto 2" 
+          imgurl="https://images3.alphacoders.com/941/94135.jpg"/>
+
+        </FlexWrapper>
     </div>
   );
 }
